a4d1da88cf317b062561c0b5522e5fd50eeb5083
Lesly Brisson <brissonlesly@gmail.com>
2022-06-30 20:46:54 +0000

    Looping techniques

---
+++ fav_char.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+itachi = {
+        "name": "Itachi",
+        "age": "21",
+        "special power": "sharingan",
+        "elemental powers": "fire",
+        "home village": "leaf",
+        "skills": ["hand2hand", "genjutsu", "ninja skills", "fire jutsu", "uchiha skills", "badass"]
+        }
+
+print(char)
