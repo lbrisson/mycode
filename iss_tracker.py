83e0eb977a04f377c627ae1cec65e96bfa45b279
Lesly Brisson <brissonlesly@gmail.com>
2022-07-06 15:44:00 +0000

    api challenge

---
+++ iss_tracker.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Returning the location of the ISS in latitude/longitude"""
+import requests
+import datetime
+
+URL= "http://api.open-notify.org/iss-now.json"
+def main():
+    resp= requests.get(URL).json()
+    lon = resp#!/usr/bin/env pyt#!/usr/bin/env python3
+"""Returning the location of the ISS in latitude/longitude"""
+import requests
+import datetime
+
+URL= "http://api.open-notify.org/iss-now.json"
+def main():
+    resp= requests.get(URL).json()
+    lon = resp
+if __name__ == "__main__":
+    main()
+~                                                                                    
+~                               hon3
+"""Returning the location of the ISS in latitude/longitude"""
+import requests
+import datetime
+
+URL= "http://api.open-notify.org/iss-now.json"
+def main():
+    resp= requests.get(URL).json()
+    lon = resp
+if __name__ == "__main__":
+    main()
+~                                                                                                
+~                               
+if __name__ == "__main__":
+    main()
